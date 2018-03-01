f2e98aaf4462984fc276a9e774f07fcd61594a54
Mark Jordan <mjordan@sfu.ca>
2018-02-28 21:04:40 -0800

    Catchup commit, including adding a sample httpful script.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Islandora CLAW REST Ingester
 
-Proof of concept script to ingest a small batch of nodes and accompanying images using Islandora CLAW's REST interface. Based on https://github.com/Islandora-CLAW/islandora/pull/76.
+Proof of concept script to ingest a batch of nodes and accompanying JPEG images using Islandora CLAW's REST interface. REST workflow is based on that described in https://github.com/Islandora-CLAW/islandora/pull/76.
 
 ## Installation
 
@@ -10,5 +10,49 @@ Proof of concept script to ingest a small batch of nodes and accompanying images
 
 ## Usage
 
+You may need to adjust the following variables at the top of `ingest.php`:
+
+```
+$host = 'http://localhost:8000';
+$credentials = array('admin', 'islandora');
+$input_dir = 'input_data';
+$csv_file = $input_dir . '/metadata.csv';
+```
+
+To execute the script using the sample data, run this:
+
 `php ingest.php`
 
+## Ingesting your own images
+
+You can load your own nodes and accompanying imamges as long as the nodes have the `islandora_image` content type and the images are JPEGs.
+
+The directory that contains the data to be ingested is arranged like this:
+
+```
+your_folder/
+├── IMG_1410.JPG
+├── IMG_2549.JPG
+├── IMG_2940.JPG
+├── IMG_2958.JPG
+├── IMG_5083.JPG
+└── metadata.csv
+```
+
+The names of the images can take any form you want since they are included in the CSV file (which can also be named whatever you want). That file must contain three columns, `File`, `Title`, and `Description`, corresponding to the fields that the default CLAW `islandora_image` content type has. The `File` column contains the full filename of the JPEG, and the other two columns contain the corresponding values for your nodes.
+
+If you load you own data, be sure that the values of the `$input_dir` and `$csv_file` point to the right paths.
+
+## Maintainer
+
+* [Mark Jordan](https://github.com/mjordan)
+
+## Development and feedback
+
+* If you discover a bug, or have a use case not documented here, open an issue.
+* If you want to open a pull request, open an issue first.
+  * By opening a pull request, you agree to placing your contribution into the public domain.
+
+## License
+
+The Unlicense
--- ingest.php
+++ ingest.php
@@ -1,34 +1,39 @@
 <?php
 
-require 'vendor/autoload.php';
-use League\Csv\Reader;
-use GuzzleHttp\Client as GuzzleClient;
-
+// You may need to adjust these variables.
 $host = 'http://localhost:8000';
 $credentials = array('admin', 'islandora');
 $input_dir = 'input_data';
 $csv_file = $input_dir . '/metadata.csv';
 
+// You do not need to adjust anything below this line.
+
+require 'vendor/autoload.php';
+use League\Csv\Reader;
+use GuzzleHttp\Client as GuzzleClient;
+
 $csv = Reader::createFromPath($csv_file);
 $records = $csv->fetchAssoc();
 
-$client = new GuzzleClient();
+$client = new GuzzleClient(array('http_errors' => false));
 
 foreach ($records as $record) {
     $node = array(
-        array('type' => array('target_id' => 'islandora_image', 'target_type' => 'node_type')),
-        array('title' => array('value' => $record['Title'])),
-        array('field_description' => array('value' => $record['Description'])),
+        'type' => array(array('target_id' => 'islandora_image', 'target_type' => 'node_type')),
+        'title' => array(array('value' => $record['Title'])),
+        'field_description' => array(array('value' => $record['Description'])),
     );
     $json = json_encode($node);
 
     // Create the node.
     $endpoint = $host . '/node';
-    $node_response = $client->request('POST', $endpoint, ['auth' => $credentials, 'json' => $json]);
+
+    // This request results in a 403.
+    $node_response = $client->request('POST', $endpoint, ['auth' => $credentials, 'headers' => array('Content-Type' => 'application/json'), 'body' => $json]);
+
     // Add the JPEG file.
     if ($node_response->getStatusCode() == 201) {
-        $node_uri = $response->getHeader('Location');
-        // curl -v -u admin:islandora -H "Content-Type: image/jpeg" -H "Content-Disposition: attachment; filename=\"test.jpeg\"" --data-binary @test.jpeg http://localhost:8000/node/4/media/field_web_content/add/web_content
+        $node_uri = $node_response->getHeader('Location');
         $file_path = $input_dir . '/' . $record['File'];
         if (file_exists($file_path)) {
             $pathinfo = pathinfo($path);
@@ -38,11 +43,11 @@ foreach ($records as $record) {
             $file_response = $client->request('POST', $endpoint, ['auth' => $credentials, 'headers' => $headers, 'body' => $image_file_contents]);
         }
         else {
-            print "File " . $record['File'] . " does not exist\n";
+            print "File " . $record['File'] . " does not exist; node " . $node_uri . " created, but no file attached.\n";
         }
     }
     else {
-        print "Node " . $record['Title'] . " not created (HTTP response code " . $response->getStatusCode() . "\n";
+        print 'Node "' . $record['Title'] . '" not created (HTTP response code ' . $node_response->getStatusCode() . ")\n";
     }
 }
 
